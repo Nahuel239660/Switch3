8632475f5a2ec62543c995163e2a8fdde7b4fad8
Nahuel239660 <92473194+Nahuel239660@users.noreply.github.com>
2022-11-28 23:41:20 -0300

    commit trabajo anda

---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+export default {
+    primary: '#adb5bd',
+    secondary: '#ced4da',
+    buttons: '#6c757d',
+    text: '#f8f9fa',
+    backgroundColor: '#212529',
+    white: 'white',
+    black: 'black',
+    gray: 'gray',
+}
+
